f102f96e87c9f72c54529d3985504065f6597994
Nico Schlömer <nico.schloemer@gmail.com>
2021-06-15 19:11:09 +0200

    chartjs: error message

--- src/content_script_chartjs.js
+++ src/content_script_chartjs.js
@@ -6,20 +6,24 @@ const renderChartjs = () => {
   collection = document.querySelectorAll("pre[lang='chartjs']");
 
   for (element of collection) {
-    obj = JSON.parse(element.textContent);
-
-    canvas = document.createElement("canvas");
-    if (obj.canvas !== undefined) {
-      for (const [key, value] of Object.entries(obj.canvas)) {
-        canvas.setAttribute(key, value);
+    try {
+      obj = JSON.parse(element.textContent);
+      canvas = document.createElement("canvas");
+      if (obj.canvas !== undefined) {
+        for (const [key, value] of Object.entries(obj.canvas)) {
+          canvas.setAttribute(key, value);
+        }
       }
+      element.parentNode.appendChild(canvas);
+
+      new Chart(canvas, obj.config);
+
+      // remove the code block
+      element.remove();
+    } catch (err) {
+      element.setAttribute("style", "color:red");
+      element.textContent = err.message;
     }
-    element.parentNode.appendChild(canvas);
-
-    new Chart(canvas, obj.config);
-
-    // remove the code block
-    element.remove();
   }
 };
 
